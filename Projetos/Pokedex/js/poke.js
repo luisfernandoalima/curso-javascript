86086b0c60d396b49d44248d8e0e899c10c2ce02
Luis Fernando A. Lima <90354100+LuisFernandoALima@users.noreply.github.com>
2022-05-17 21:47:34 -0300

    Seletor funcionando

--- Projetos/Pokedex/js/poke.js
+++ Projetos/Pokedex/js/poke.js
@@ -1,5 +1,11 @@
+var LastCard = ''
+
+
+
+
 function Dex() {
     var Card = document.querySelector('input[name="Pokemon"]:checked').value;
+    var FirstCard = document.querySelector('.D' + Card)
     var PkName = document.querySelector('#Name' + Card)
     var PkType = document.querySelector('#Type' + Card)
     var nameDex = document.querySelector('#NomePokemon')
@@ -13,6 +19,19 @@ function Dex() {
     var Sp_Attack = document.querySelector('#Sp_Attack')
     var Sp_Defense = document.querySelector('#Sp_Defense')
     var Speed = document.querySelector('#Speed')
+
+    //Marcando o bixo
+    FirstCard.classList.add('Marcado')
+    if (LastCard == '') {
+        LastCard = FirstCard
+    }
+    if (FirstCard != LastCard) {
+        LastCard.classList.remove('Marcado')
+        LastCard = FirstCard
+    }
+
+
+    console.log(LastCard)
 
 
     //Colocando as características na tela
@@ -42,6 +61,33 @@ function Dex() {
         Sp_Attack.value = 100
         Sp_Defense.value = 100
         Speed.value = 80
+
+        //Botão Mega
+        nameDex.innerHTML += '     <button id="MegaButton" class="MegaActive"><img src="img/MegaSymbol.png" alt="" id="Megaimg"></button>'
+    } else if (Card == 'P004') {
+        HP.value = 39
+        Attack.value = 52
+        Defense.value = 43
+        Sp_Attack.value = 60
+        Sp_Defense.value = 50
+        Speed.value = 65
+    } else if (Card == 'P005') {
+        HP.value = 58
+        Attack.value = 64
+        Defense.value = 58
+        Sp_Attack.value = 80
+        Sp_Defense.value = 65
+        Speed.value = 80
+    } else if (Card == 'P006') {
+        HP.value = 78
+        Attack.value = 84
+        Defense.value = 78
+        Sp_Attack.value = 109
+        Sp_Defense.value = 85
+        Speed.value = 100
+
+        //Botão Mega
+        nameDex.innerHTML += '     <button id="MegaButton" class="MegaActive"><img src="img/MegaSymbol.png" alt="" id="Megaimg"></button>'
     }
 
 }
